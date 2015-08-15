60c343c2caabb6e1a97bb303eb4b2cf597e28b81
ppwozniak <pawel.wozniak.pl@gmail.com>
2015-08-15 16:52:48 +0200

    Modifying pom.xml (adding plugins etc.), adding first (empty) class and modifying .gitignore file

---
+++ src/main/java/pl/ppwozniak/hipchat/api2/Test.java
@@ -0,0 +1,16 @@
+/**
+ * *************************************************
+ * Projekt: hipchat-api2
+ * <p/>
+ * Autor: Paweł Woźniak
+ * <p/>
+ * Data utworzenia: 2015-08-15
+ * *************************************************
+ */
+package pl.ppwozniak.hipchat.api2;
+
+/**
+ * @author wp
+ */
+public class Test {
+}
